1c6136f5ddfbbd5e9d85a7b4f8ba7392b50514df
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2019-01-04 12:01:56 +0100

    Added margin and padding for the main content

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="team">
-    <h1>TEAM</h1>
+    <h1 class="grey--text">TEAM</h1>
+
+    <v-container class="my-5">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolor ab, commodi minima quasi nesciunt suscipit odit est quis deleniti quae amet rem, dolores architecto tempore libero voluptates quidem. Eligendi!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium molestiae excepturi natus odio, animi dolorem inventore incidunt qui praesentium totam nesciunt pariatur, veritatis enim ex reiciendis libero laboriosam sequi mollitia.</p>
+    </v-container>
+
   </div>
 </template>
 
