1becbcc5051dd5e968a7ba7d42ad9f70f5949cfc
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2019-01-04 11:33:50 +0100

    Created sidebar links and routes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -14,7 +14,16 @@
     </v-toolbar>
 
     <v-navigation-drawer app v-model="drawer" class="indigo">
-      <p>TEST</p>
+      <v-list>
+        <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-tile-action>
+            <v-icon class="white--text">{{ link.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title class="white--text">{{ link.text }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
     </v-navigation-drawer>
   </nav>
 </template>
@@ -24,7 +33,12 @@
 export default {
   data() {
     return {
-      drawer: false
+      drawer: false,
+      links: [
+        { icon: 'dashboard', text: 'DASHBOARD', route: '/'},
+        { icon: 'folder', text: 'MY PROJECTS', route: '/projects'},
+        { icon: 'person', text: 'TEAM', route: '/team'}
+      ]
     }
   }
 }
