055c14687c4d6dcdd56e54623dfc90b729484e23
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2019-01-08 22:13:13 +0100

    Created some data to show for the team members

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -3,8 +3,7 @@
     <h1 class="grey--text">TEAM</h1>
 
     <v-container class="my-5">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolor ab, commodi minima quasi nesciunt suscipit odit est quis deleniti quae amet rem, dolores architecto tempore libero voluptates quidem. Eligendi!</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium molestiae excepturi natus odio, animi dolorem inventore incidunt qui praesentium totam nesciunt pariatur, veritatis enim ex reiciendis libero laboriosam sequi mollitia.</p>
+
     </v-container>
 
   </div>
@@ -12,6 +11,16 @@
 
 <script>
 export default {
-
+  data() {
+    return {
+      team: [
+        { name: 'Edgardo Pinto-Escalier', role: 'Web Developer' },
+        { name: 'Tom Williams', role: 'UX/UI Designer' },
+        { name: 'Laura Thomas', role: 'Senior Back-End Developer' },
+        { name: 'Paul Jones', role: 'SEO Specialist' },
+        { name: 'Ann Wilson', role: 'Sales Manager' }
+      ]
+    }
+  }
 }
 </script>
