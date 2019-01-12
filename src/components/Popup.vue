032c22de7a24dc0f736e9c891889f5d39e7ac88a
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2019-01-12 12:55:45 +0100

    Added a Popup dialog with a button on the sidebar for the form

---
+++ src/components/Popup.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-dialog max-width="600px">
+    <v-btn flat slot="activator" class="success">ADD NEW PROJECT</v-btn>
+    <v-card>
+      <v-card-title>
+        <h2>ADD A NEW PROJECT</h2>
+      </v-card-title>
+      
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
