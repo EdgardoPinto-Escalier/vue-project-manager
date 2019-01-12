e72d7e12ea76b611e6d41af3090d1a54d92d00e8
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2019-01-12 13:20:22 +0100

    Added a form inside  the Popup component

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -5,14 +5,25 @@
       <v-card-title>
         <h2>ADD A NEW PROJECT</h2>
       </v-card-title>
-      
+      <v-card-text>
+        <v-form class="px-3">
+          <v-text-field label="PROJECT TITLE" v-model="title" prepend-icon="title"></v-text-field>
+          <v-textarea label="PROJECT DESCRIPTION" v-model="description" prepend-icon="description"></v-textarea>
+          <v-btn flat class="success mx-0 mt-3">ADD PROJECT</v-btn>
+        </v-form>
+      </v-card-text>
     </v-card>
   </v-dialog>
 </template>
 
 <script>
 export default {
-  
+  data() {
+    return {
+      title: '',
+      description: ''
+    }
+  }
 }
 </script>
 
